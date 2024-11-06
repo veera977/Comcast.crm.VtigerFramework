9052fa23962e7de46a43fad15a88175fe55680dc
USER <USER@DESKTOP-7SM6G84>
2024-11-06 12:16:28 +0530

    pushingg new class to global repository

---
+++ Automation_FrameWORK2_dummy/src/main/java/Com/comcast/crm/ObjectrepositoryUtility/HomePage2.java
@@ -0,0 +1,23 @@
+package Com.comcast.crm.ObjectrepositoryUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage2 {
+	WebDriver driver;
+
+	public HomePage2(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(linkText = "Organizations")
+	private WebElement orglink;
+	@FindBy(linkText = "Products")
+	private WebElement prolink;
+	public WebElement getProlink() {
+		return prolink;
+	}
+}
