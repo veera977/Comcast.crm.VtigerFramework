7540e377a6148bcf2d82a51f14333f6051471809
USER <USER@DESKTOP-7SM6G84>
2024-11-07 10:39:37 +0530

    changes

---
+++ Automation_FrameWORK2_dummy/src/main/java/Com/comcast/crm/ObjectrepositoryUtility/class1.java
@@ -0,0 +1,13 @@
+package Com.comcast.crm.ObjectrepositoryUtility;
+
+public class class1 {
+public static void main(String[] args) {
+		
+		System.out.println("hii");
+		System.out.println("hello");
+		System.out.println("bye");
+	}
+
+}
+
+
